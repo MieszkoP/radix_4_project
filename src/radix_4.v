bc1c9b21a7163d0cdab6cc77878296a7d0e79374
MieszkoP <mieszko.pasierbek@gmail.com>
2025-06-01 14:28:11 +0200

    Poprawiony kod, mniej stanow

--- src/radix_4.v
+++ src/radix_4.v
@@ -37,6 +37,12 @@ module radix_4(clock, reset, start, x_value, y_value, total_product, ready);
     parameter S0 = 4'h00, S1 = 4'h01, S2 = 4'h02, S3 = 4'h03, S4 = 4'h04, S5 = 4'h05,
     S6 = 4'h06, S7 = 4'h07, S8 = 4'h08, S9 = 4'h09, S10 = 4'h0a,
     S11 = 4'h0b, S12 = 4'h0c, S13 = 4'h0d;
+    
+    always @(posedge clock)
+    begin
+        
+    end
+    
     always @(posedge clock)
     begin
         if(reset==1)
@@ -45,6 +51,7 @@ module radix_4(clock, reset, start, x_value, y_value, total_product, ready);
                 ready <= 0;
                 partial_product <= 16'd0;
                 total_product <= 16'd0;
+                iter <= 0;
             end
          else
             begin
@@ -53,42 +60,44 @@ module radix_4(clock, reset, start, x_value, y_value, total_product, ready);
                 if(start ==1) current_state <= S1;
             end
             S1: begin
-                iter <= 0;
-                current_state <= S2;
+                if(iter < 5) current_state <= S2; else current_state <= S7;
             end
             S2: begin
-                if(iter < 5) current_state <= S3; else current_state <= S11;
+                partial_product <= y_value<<(iter<<1);
+                if((x_value_exp[iter<<1] == x_value_exp[(iter<<1)+1]) && (x_value_exp[(iter<<1)+1] == x_value_exp[(iter<<1)+2]))
+                    current_state <= S6;
+                else 
+                begin
+                    if(1== x_value_exp[(iter<<1)+2])
+                        current_state <= S3;
+                    else 
+                    begin
+                        if(x_value_exp[(iter<<1)]== x_value_exp[(iter<<1)+1])
+                            current_state <= S4;
+                        else 
+                        begin
+                            current_state <= S5;
+                        end
+                    end
+                end
             end
             S3: begin
-                partial_product <= y_value<<(iter<<1);
-                current_state <= S4;
+                partial_product <= -partial_product;
+                if(x_value_exp[(iter<<1)+1] == x_value_exp[iter<<1]) current_state <= S4; else current_state <= S5;
             end
             S4: begin
-                if((x_value_exp[iter<<1] == x_value_exp[(iter<<1)+1]) && (x_value_exp[(iter<<1)+1] == x_value_exp[(iter<<1)+2])) current_state <= S10; else current_state <= S5;
+                partial_product <= partial_product << 1;
+                current_state <= S5;
             end
             S5: begin
-                if(x_value_exp[(iter<<1)+2] == 1) current_state <= S6; else current_state <= S7;
+                total_product <= partial_product + total_product;
+                current_state <= S6;
             end
             S6: begin
-                partial_product <= -partial_product;
-                current_state <= S7;
+                iter <= iter+1;
+                if(iter +1 < 5) current_state <= S2; else current_state <= S7; //Iter-1, bo nieblokujące przypisanie
             end
             S7: begin
-                if(x_value_exp[(iter<<1)+1] == x_value_exp[iter<<1]) current_state <= S8; else current_state <= S9;
-            end
-            S8: begin
-                partial_product <= partial_product << 1;
-                current_state <= S9;
-            end
-            S9: begin
-                total_product <= partial_product + total_product;
-                current_state <= S10;
-            end
-            S10: begin
-                iter <= iter+1;
-                current_state <= S2;
-            end
-            S11: begin
                 ready <= 1;
             end
             endcase
